d4ad1f36897f7e7dd3979fbd3ab592a9a1414371
sarjanjebon46 <43559174+sarjanjebon46@users.noreply.github.com>
2018-09-25 09:57:01 +0800

    Add files via upload

---
+++ Projekjebontua.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekjebontua;
+
+/**
+ *
+ * @author User
+ */
+public class Projekjebontua {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+		
+		$brand=$_POST['brand'];
+		$noInvoice=$_POST['noInvoice'];
+		$date=$_POST['date'];
+		$noPlat=$_POST['noPlat'];
+		$size=$_POST['size'];
+		$price=$_POST['price'];
+		$bil=$_POST['bil'];
+    }
+    
+}
